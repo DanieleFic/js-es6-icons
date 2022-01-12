b73088378f04877bf2b3d83240f4b822d8d38e48
DanieleFic <reb_ficus22@hotmail.it>
2022-01-12 15:52:35 +0100

    inserite schede tramite JS

--- data.js
+++ data.js
@@ -4,127 +4,129 @@ const schede =[
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: 'orange'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: 'orange'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: 'orange'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: 'orange'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'indigo'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'indigo'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'indigo'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'indigo'
 	}
 ];
 
 console.log("ciao")
 
+//creo una function che mi crea i post
 function creazioneSchede( schede ){
     document.querySelector(".row").innerHTML +=
     `<div class="col-2 mx-4 py-3">
-	<div class="p-2  rounded bg-light border border-dark d-flex flex-column align-items-center justify-content-center">
-		<i class="${schede.family} ${schede.prefix}${schede.name}" style="${schede.color}"></i>
-		<span>${schede.name}</span> 
-	</div>
-  </div> `           
+		<div class="p-2  rounded bg-light border border-dark d-flex flex-column align-items-center justify-content-center">
+			<i class="${schede.family} ${schede.prefix}${schede.name}" style="color:${schede.color}"></i>
+			<span>${schede.name}</span> 
+		</div>
+  	</div> `           
   }
 
-  for(let i=0; i < schede.length; i++){
-    creazioneSchede( schede[i] )
+//faccio comparire le schede nel DOM tramite un ciclo for usando una function
+for(let i=0; i < schede.length; i++){
+	creazioneSchede( schede[i] )
 }
